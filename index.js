35d146243de89081d6272f930dfc84740d5e56c8
Himanshu <hudixt@gmail.com>
2019-09-03 15:00:46 +0530

    Add syste colors

---
+++ index.js
@@ -0,0 +1,16 @@
+/*
+  Exports of this package.
+
+  Date - 3 Sep 2019
+  Author- Himanshu Dixit
+*/
+
+import colors from './src/tokens/colors';
+
+const moduleExports = {
+    tokens:{
+        colors: colors
+    }
+}
+
+export default moduleExports;
